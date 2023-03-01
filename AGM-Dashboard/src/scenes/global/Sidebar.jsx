fd17baa9113b18f07e6d262cbebbace014f5f246
Hyungmin Jeon <hyungmin.jeon@bellevuecollege.edu>
2023-03-01 13:04:30 -0800

    npm install react-scripts --legacy-peer-deps

--- AGM-Dashboard/src/scenes/global/Sidebar.jsx
+++ AGM-Dashboard/src/scenes/global/Sidebar.jsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../assets/user.png`}
+                  src={`../../assets/garden.jpg`}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
