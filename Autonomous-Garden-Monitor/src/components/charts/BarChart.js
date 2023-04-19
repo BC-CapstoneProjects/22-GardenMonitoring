38e0ac75ddd7d8e31fa412c626be571eca3132b7
BenjaminVC <benjamin.van-cise@bellevuecollege.edu>
2023-04-19 15:52:01 -0700

    added .gitkeep to upload empty gardens folder

--- Autonomous-Garden-Monitor/src/components/charts/BarChart.js
+++ Autonomous-Garden-Monitor/src/components/charts/BarChart.js
@@ -1,42 +1,76 @@
-// ./components/BarChart.js
-import React from "react";
-import Chart from "chart.js/auto";
+import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
+import plants from '../Plants/PlantDescriptions';
+import { plugins } from "chart.js";
 
-// Chart.defaults.color = () => {
-// 	debugger;
-// 	const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-//   return isDarkMode ? 'stone' : 'white'
-// cmd='Mosaic Disease',
-// cbb='Bacterial Blight',
-// cgm='Green Mite',
-// cbsd='Brown Streak Disease',
-// healthy='Healthy',
-// unknown='Unknown'
-// }
+const countDiseaseOccurrences = (diseaseLabels) => {
+  const diseaseCount = {};
+  diseaseLabels.forEach((disease, index) => {
+    if (disease) {
+      if (!diseaseCount[disease]) {
+        diseaseCount[disease] = { count: 0, plantNames: [] };
+      }
+      diseaseCount[disease].count += 1;
+      diseaseCount[disease].plantNames.push(plants[index].name);
+    }
+  });
+  return diseaseCount;
+};
 
-const BarChart = () => {
-  const labels = ["Mosaic Disease", "Bacterial Blight", "Green Mite", "Brown Streak Disease", "Healthy", "Unknown"];
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "Overall Garden Health",
-        backgroundColor: ["rgba(255, 99, 132, 0.8)",
-        "rgba(54, 162, 235, 0.8)",
-        "rgba(255, 206, 86, 0.8)",
-        "rgba(75, 192, 192, 0.8)",
-        "rgba(153, 102, 255, 0.8)",
-        "rgba(255, 159, 64, 0.8)",],
-        borderColor: "rgb(255, 99, 132)",
-        data: [0.5, 1, 1, 2, 3, 0.2],
-      },
-    ],
+const BarChart = ({ data, selectedGarden }) => {
+  const [chartData, setChartData] = useState(null);
 
-  };
+  useEffect(() => {
+    if (data && data.length > 0) {
+      const diseaseLabels = data.map((scan) => scan[0].disease);
+      const diseaseCount = countDiseaseOccurrences(diseaseLabels);
+      const chartData = {
+        labels: Object.keys(diseaseCount),
+        datasets: [
+          {
+            label: `Disease Occurrences for ${selectedGarden}`,
+            data: Object.values(diseaseCount).map(({ count }) => count),
+            backgroundColor: "rgba(75, 192, 192, 0.6)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+            custom: Object.values(diseaseCount).map(({ plantNames }) => plantNames),
+          },
+        ],
+      };
+
+      setChartData(chartData);
+    }
+  }, [data, selectedGarden]);
+
   return (
-    <div class="bg-stone-600/0">
-      <Bar data={data} />
+    <div className="bar-chart">
+      {chartData ? (
+        <Bar
+          data={chartData}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+            plugins: {
+              tooltip: {
+                callbacks: {
+                  title: function (context) {
+                    const dataIndex = context[0].dataIndex;
+                    const diseaseLabel = context[0].label;
+                    const plantNames = chartData.datasets[0].custom[dataIndex];
+                    return `Affected plants: \n-${plantNames.join('\n-')}`;
+                  },
+                },
+              },
+            },
+          }}
+        />
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
