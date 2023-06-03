a9720f6523c2b5d0665bea335d6618b7eb8beb76
BenjaminVC <benjamin.van-cise@bellevuecollege.edu>
2023-06-03 16:55:07 -0700

    test for tedo and i

--- Autonomous-Garden-Monitor/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ Autonomous-Garden-Monitor/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "dev8366e037": {
+    "dev68d72dd8": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
